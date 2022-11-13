30a9244135d5b99b4283b9f45d617263f803fab8
Harry Filis <harry.filis@yahoo.gr>
2022-11-13 13:18:22 +0200

    Update mlp.py

--- NNets-Asgmt1/MLP/mlp.py
+++ NNets-Asgmt1/MLP/mlp.py
@@ -5,8 +5,8 @@ from torch import  nn as nn
 # Simple MultyLayer Network from Torch
 # It has Convolutional and Dense layers
 class ForwardMLPNet(nn.Module):
-    def __init__(self)
-        super().__init__():
+    def __init__(self):
+        super().__init__()
         self.conv_layer_1 = nn.Conv2d(3,6,5)
         self.max_pooling = nn.MaxPool2d(2,2)
         self.conv_layer_2 = nn.Conv2d(6,16,5)
