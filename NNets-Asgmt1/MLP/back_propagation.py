e2c0a66abe7e9526bf0c050ef2c3c423164a3be3
Harry Filis <harry.filis@yahoo.gr>
2022-11-25 13:24:51 +0200

    Create loss function and move Knn Report to right folder

--- NNets-Asgmt1/MLP/back_propagation.py
+++ NNets-Asgmt1/MLP/back_propagation.py
@@ -13,8 +13,17 @@ def training_step(epoch_index,tb_writer):
     current_loss = 0.
     last_loss = 0.
     
-    for i,data in enumerate(train_loader)
+    for i,data in enumerate(train_loader):
 
 
 
 
+# create an back propagation algorithm function for cifar10 dataset```
+# The function should return a dictionary containing the following keys: 
+# 1. training_loss: list of training loss at each epoch
+# 2. training_accuracy: list of training accuracy at each epoch
+# 3. test_loss: list of test loss at each epoch
+# 4. test_accuracy: list of test accuracy at each epoch
+# 5. epoch_time: list of epoch time at each epoch
+# 6. total_time: total time taken for training
+def back_propagation():
---
+++ NNets-Asgmt1/MLP/loss_optimzer.py
@@ -0,0 +1,17 @@
+import torch.optim as optim 
+import torch.nn as nn 
+
+def loss_function():
+    creterion = nn.CrossEntropyLoss()
+    return creterion
+
+def optimizer(net,type,lr):
+    match type:
+        case 'SGD':
+            return optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+        case 'Adam':
+            return optim.Adam(net.parameters(), lr=lr))        
+        case 'RMSprop':
+            # define rh0 with initial value 0.9 in order to have a more robust training because past is not too much important
+            return optim.RMSprop(net.parameters(), lr=lr,rho=0.9, eps=1e-08, weight_decay=0, momentum=0.9, centered=False)
+            
